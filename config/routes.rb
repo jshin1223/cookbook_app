85143f0173ab5439243dd16ebf2f89ff418dbdaf
jshin1223 <jshin1223@gmail.com>
2019-09-24 12:31:39 -0500

    update and delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/recipes' => 'recipes#index'
-    post '/recipes' => 'recipe#create'
+    post '/recipes' => 'recipes#create'
     get '/recipes/:id' => 'recipes#show'
     patch '/recipes/:id' => 'recipes#update'
     delete '/recipes/:id' => 'recipes#destroy'
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     post '/users' => 'users#create'
 
     post '/sessions' => 'sessions#create'
+
   end
 end
 
